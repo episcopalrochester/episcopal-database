e2a8dcbd18cd2d914b32db0a0d2821178f15ccfb
Matt Townsend <matthew@episcopalrochester.org>
2014-10-20 10:27:14 -0500

    adding batch script for mailchimp merge

---
+++ run_mm_batch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/php53/bin/php /home4/edor/drush/drush.php -r /home4/edor/public_html/data.episcopalrochester.org -l data.episcopalrochester.org php-eval 'mailchimp_merge_run_batch()'
